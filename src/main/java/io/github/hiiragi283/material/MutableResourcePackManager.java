f9116f6a25340bad49a76d40af7b830b762ee911
Hiiragi Russell Tsubasa <97942736+Hiiragi283@users.noreply.github.com>
2024-03-12 15:54:23 +0900

    Removed runtime packs

--- src/main/java/io/github/hiiragi283/material/MutableResourcePackManager.java
+++
@@ -1,7 +0,0 @@
-package io.github.hiiragi283.material;
-
-import net.minecraft.resource.ResourcePackProvider;
-
-public interface MutableResourcePackManager {
-    void ht_materials$addPackProvider(ResourcePackProvider provider);
-}
